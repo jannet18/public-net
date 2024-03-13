f95082d1c55684f901290566089555dffb8448e8
jannet18 <janetngei18@gmail.com>
2024-03-13 22:17:55 +0300

    internet speed route, found bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # root "places#index"
- 
+# root "places#index"
+  # get "/new-internet-speed", to: "/internet_speed"
+  resources :speed, path: "/new-internet-speed", to: "places/internet_speeds/new"
   namespace :api do
     namespace :v1 do
-      resources :places, path: "/places"
-      resources :speed, path: "/new-internet-speed", to "/internet_speed#new"
+      resources :places, path: "/places" 
+
     end
-  end
-  # get "api/v1/places" to "api/v1/places#index"
+  end 
 end
 
